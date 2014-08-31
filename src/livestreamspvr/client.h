8b1b781f3028889285c64269c4f52f8383c5bc30
zcsizmadia <zoltan_csizmadia@yahoo.com>
2014-08-30 22:03:43 -0500

    Update version

--- src/livestreamspvr/client.h
+++ src/livestreamspvr/client.h
@@ -30,9 +30,9 @@
 #include "libXBMC_pvr.h"
 #include "libXBMC_gui.h"
 
-#define PVR_CLIENT_VERSION      "1.0.0"
-#define XML_FILE_NAME          "livestreams.xml.cache"
-#define TVG_FILE_NAME          "xmltv.xml.cache"
+#define PVR_CLIENT_VERSION    "1.0.1"
+#define XML_FILE_NAME         "livestreams.xml.cache"
+#define TVG_FILE_NAME         "xmltv.xml.cache"
 
 /*!
  * @brief PVR macros for string exchange
